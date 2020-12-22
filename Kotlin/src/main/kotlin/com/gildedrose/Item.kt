d609afbaee84fca4b21dd0472a95df438ce54bcb
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-22 21:13:23 +0100

    test: Create golden master

--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -4,4 +4,25 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Item
+
+        if (name != other.name) return false
+        if (sellIn != other.sellIn) return false
+        if (quality != other.quality) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + sellIn
+        result = 31 * result + quality
+        return result
+    }
 }
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,9 +1,36 @@
 package com.gildedrose
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
+
+    private val items = arrayOf(
+        Item("+5 Dexterity Vest", 10, 20), //
+        Item("Aged Brie", 2, 0), //
+        Item("Elixir of the Mongoose", 5, 7), //
+        Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        Item("Conjured Mana Cake", 3, 6)
+    )
+
+    private val legacyItems = arrayOf(
+        Item("+5 Dexterity Vest", 10, 20), //
+        Item("Aged Brie", 2, 0), //
+        Item("Elixir of the Mongoose", 5, 7), //
+        Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        Item("Conjured Mana Cake", 3, 6)
+    )
 
     @Test
     fun foo() {
@@ -11,9 +38,18 @@ internal class GildedRoseTest {
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals("foo", app.items[0].name)
-
     }
 
+    @Test
+    internal fun `Legacy Gilded Rose must have the same behaviour then the new Gilded Rose`() {
+        val gildedRose = GildedRose(items)
+        val legacyGildedRose = LegacyGildedRose(legacyItems)
+
+        for (i in 0..100) {
+            gildedRose.updateQuality()
+            legacyGildedRose.updateQuality()
+
+            assertArrayEquals(legacyItems, items)
+        }
+    }
 }
-
-
