2db9a9438e3e458e35fa92598b76be37c1729673
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-22 21:44:51 +0100

    refactor: Use increment and decrement syntaxe

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -12,44 +12,44 @@ class GildedRose(private val items: Array<Item>) {
         when (item.name) {
             "Aged Brie" -> {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality++
                 }
 
-                item.sellIn = item.sellIn - 1
+                item.sellIn--
 
                 if (item.sellIn < 0 && item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality++
                 }
             }
             "Backstage passes to a TAFKAL80ETC concert" -> {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality++
 
                     if (item.sellIn < 11 && item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality++
                     }
 
                     if (item.sellIn < 6 && item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality++
                     }
                 }
 
-                item.sellIn = item.sellIn - 1
+                item.sellIn--
 
                 if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality
+                    item.quality = 0
                 }
             }
             "Sulfuras, Hand of Ragnaros" -> {
             }
             else -> {
                 if (item.quality > 0) {
-                    item.quality = item.quality - 1
+                    item.quality--
                 }
-                item.sellIn = item.sellIn - 1
+                item.sellIn--
 
                 if (item.sellIn < 0 && item.quality > 0) {
-                    item.quality = item.quality - 1
+                    item.quality--
                 }
             }
         }
