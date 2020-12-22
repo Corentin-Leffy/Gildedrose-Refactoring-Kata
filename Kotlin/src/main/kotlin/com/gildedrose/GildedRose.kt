8c90f42505e4620abc8c3fcf23c3b9687d5c93f0
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-22 21:36:13 +0100

    refactor: Extract 'Sulfuras' behaviour (and default item)

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -39,21 +39,17 @@ class GildedRose(private val items: Array<Item>) {
             if (item.sellIn < 0) {
                 item.quality = item.quality - item.quality
             }
+        } else if (item.name == "Sulfuras, Hand of Ragnaros") {
+
         } else {
             if (item.quality > 0) {
-                if (item.name != "Sulfuras, Hand of Ragnaros") {
-                    item.quality = item.quality - 1
-                }
+                item.quality = item.quality - 1
             }
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
-            }
+            item.sellIn = item.sellIn - 1
 
             if (item.sellIn < 0) {
                 if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
-                    }
+                    item.quality = item.quality - 1
                 }
             }
         }
