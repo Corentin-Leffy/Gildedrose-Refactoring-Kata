8d24d662064d21a2fa8dc7659de135cd7de7c064
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-22 22:25:05 +0100

    refactor: Use days threshold write in domain rules

--- Kotlin/src/main/kotlin/com/gildedrose/BackstagePasses.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/BackstagePasses.kt
@@ -3,12 +3,13 @@ package com.gildedrose
 class BackstagePasses(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
     override fun updateQuality() {
         incrementQuality()
-        if (sellIn < 11) {
+        if (sellIn <= 10) {
             incrementQuality()
         }
-        if (sellIn < 6) {
+        if (sellIn <= 5) {
             incrementQuality()
         }
+
         decrementSellIn()
 
         if (sellIn < 0) {
