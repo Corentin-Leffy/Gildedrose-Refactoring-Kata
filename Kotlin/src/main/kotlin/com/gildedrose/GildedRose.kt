f79e8aabecdaf7eec3c18af6db12cd59e508f651
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-22 22:15:05 +0100

    refactor: Use directly the method updateQuality

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,16 +4,7 @@ class GildedRose(private val items: Array<Item>) {
 
     fun incrementQuality() {
         for (item in items) {
-            update(item)
-        }
-    }
-
-    private fun update(item: Item) {
-        when (item.name) {
-            "Aged Brie" -> item.updateQuality()
-            "Backstage passes to a TAFKAL80ETC concert" -> item.updateQuality()
-            "Sulfuras, Hand of Ragnaros" -> item.updateQuality()
-            else -> item.updateQuality()
+            item.updateQuality()
         }
     }
 }
