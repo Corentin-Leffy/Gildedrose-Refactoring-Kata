0c95ba671db9f6efda70a0e7c335fe9d4bc69e48
Corentin Leffy <corentin.leffy@gmail.com>
2021-01-02 14:34:20 +0100

    refactor: Extract bonuses behaviour inside a class

--- Kotlin/src/main/kotlin/com/gildedrose/BackstagePasses.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/BackstagePasses.kt
@@ -2,6 +2,11 @@ package com.gildedrose
 
 class BackstagePasses(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
 
+    private val qualityBonuses = QualityBonuses(
+        QualityBonus(bonus = 3, days = 5),
+        QualityBonus(bonus = 2, days = 10),
+    )
+
     override fun updateQuality() {
         increaseQuality()
         decreaseSellIn()
@@ -14,13 +19,7 @@ class BackstagePasses(name: String, sellIn: Int, quality: Int) : Item(name, sell
         repeat(qualityBonus()) { super.increaseQuality() }
     }
 
-    private fun qualityBonus() = when {
-        expireInLessThan(5) -> 3
-        expireInLessThan(10) -> 2
-        else -> 1
-    }
-
-    private fun expireInLessThan(days: Int) = sellIn <= days
+    private fun qualityBonus() = qualityBonuses.findBonus { days -> sellIn <= days }
 
     private fun dropQualityToMinimum() {
         quality = Quality.MINIMUM
---
+++ Kotlin/src/main/kotlin/com/gildedrose/QualityBonuses.kt
@@ -0,0 +1,16 @@
+package com.gildedrose
+
+class QualityBonuses(
+    vararg bonus: QualityBonus,
+) : List<QualityBonus> by bonus.toList() {
+
+    fun findBonus(predicate: (Int) -> Boolean): Int =
+        firstOrNull { (_, days) -> predicate(days) }?.bonus
+            ?: DEFAULT_BONUS
+
+    companion object {
+        private const val DEFAULT_BONUS = 1
+    }
+}
+
+data class QualityBonus(val bonus: Int, val days: Int)
